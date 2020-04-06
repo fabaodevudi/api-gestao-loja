7682bbd069937758ba260cf72e9f2e970cb6d370
Fabio Pereira De Vasconcelos <5178pc_fabio@callink.com.br>
2020-04-06 16:01:36 -0300

    100%

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,13 +1,13 @@
 create database db_gestao_loja;
 use db_gestao_loja;
 create table usuario_login (
-	id_usuario_login int primary key auto_increment,
+    id_usuario_login int primary key auto_increment,
     email varchar(100),
     senha varchar(500),
     perfil varchar(20)
 );
 create table usuario (
-	id_usuario int primary key auto_increment,
+    id_usuario int primary key auto_increment,
     nome varchar(100),
     telefone varchar(14),
     cpf varchar(11),
@@ -15,17 +15,17 @@ create table usuario (
      constraint fk_usuario_usuario_login FOREIGN KEY (id_usuario_login) REFERENCES usuario_login (id_usuario_login)
 );
 create table categoria_loja (
-	id_categoria int primary key auto_increment,
+    id_categoria int primary key auto_increment,
     nome varchar(100),
     descricao varchar(500)  
 );
 
 create table loja (
-	id_loja int primary key auto_increment,
+    id_loja int primary key auto_increment,
     nome varchar(100),
     descricao varchar(500),
     localizacao varchar(50),
-	id_categoria int,
+    id_categoria int,
     id_usuario int,
     constraint fk_loja_categoria FOREIGN KEY (id_categoria) REFERENCES categoria_loja (id_categoria),
     constraint fk_loja_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
@@ -34,5 +34,3 @@ create table loja (
 insert into categoria_loja (nome, descricao) values ('TI', 'Tecnologia da Informação');
 insert into categoria_loja (nome, descricao) values ('Alimentos', 'Venda de alimentos');
 insert into categoria_loja (nome, descricao) values ('Sexshop', 'Produtos eróticos');
-
-insert into usuario(email, senha, perfil) values ('fabiodesignerml@gmail.com', '123456', 'ROLE_USUARIO');
